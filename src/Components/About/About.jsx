d397c5c500a9a12ecb6e2227b16c762120a2984b
noorejannat111 <noorejannat188@gmail.com>
2021-06-26 14:58:53 +0600

    done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css';
-import img from '../../img/me.jpg';
-import {FaFacebookSquare, FaInstagramSquare , FaGithubSquare} from 'react-icons/fa';
+
+import { FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from 'react-icons/fa';
 const About = () => {
     return (
         <>
@@ -11,15 +11,15 @@ const About = () => {
                     <h5>About Me</h5>
                     <div className="under-line"></div>
                 </div>
-                <img src={img} alt=""/>
-                <p>Hey dude, This is Shakil Babu who wanna make something good. I am a JavaScript Developer also love to C++.</p>
+
+                <p>Hello, This is Noor-E-Jannat who wanna make something good. I am a JavaScript Developer also love to React.</p>
 
                 <div className="social-flex">
-                    <a href='https://www.facebook.com/profile.php?id=100025305922873' target='_blank'> <FaFacebookSquare className='Icon' /> </a>
-                    <a href='https://www.instagram.com/shakilbabupro/' target='_blank'> <FaInstagramSquare className='Icon' /> </a>
-                    <a href='https://github.com/shakil-Babu/' target='_blank'> <FaGithubSquare className='Icon' /> </a>
+                    <a href='https://www.facebook.com/noorejannat044' target='_blank'> <FaFacebookSquare className='Icon' /> </a>
+                    <a href='https://www.instagram.com/noorejannat/' target='_blank'> <FaInstagramSquare className='Icon' /> </a>
+                    <a href='https://github.com/Noor-E-Jannat111' target='_blank'> <FaGithubSquare className='Icon' /> </a>
                 </div>
-            </section>   
+            </section>
         </>
     )
 }
