c90c394866e07bb0798042f567a2c77f5c1936de
noorejannat111 <noorejannat188@gmail.com>
2021-06-26 14:54:44 +0600

    done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Footer.css';
-import {FaFacebookSquare, FaInstagramSquare , FaGithubSquare} from 'react-icons/fa';
+import { FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from 'react-icons/fa';
 const Footer = () => {
     return (
         <>
             <footer className="footer-area ">
-                <h1>All right reserved <mark>"DAILY DOSE"</mark></h1>
+                <h1>All right reserved <mark>"Digital Diary"</mark></h1>
                 <div className="icons-flex">
-                    <a href='https://www.facebook.com/profile.php?id=100025305922873' target='_blank'> <FaFacebookSquare className='Icon' /> </a>
-                    <a href='https://www.instagram.com/shakilbabupro/' target='_blank'> <FaInstagramSquare className='Icon' /> </a>
-                    <a href='https://github.com/shakil-Babu/' target='_blank'> <FaGithubSquare className='Icon' /> </a>
+                    <a href='https://www.facebook.com/noorejannat044' target='_blank'> <FaFacebookSquare className='Icon' /> </a>
+                    <a href='https://www.instagram.com/noorejannat/' target='_blank'> <FaInstagramSquare className='Icon' /> </a>
+                    <a href='https://github.com/Noor-E-Jannat111' target='_blank'> <FaGithubSquare className='Icon' /> </a>
                 </div>
-            </footer>   
+            </footer>
         </>
     )
 }
